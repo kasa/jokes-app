596cfab44055f7b4b6ed8b69e4fdb02e15711de0
Eduardo Kasa <ekasa@redventures.com>
2023-01-16 19:34:51 -0300

    use accordion

--- src/components/JokeList.tsx
+++ src/components/JokeList.tsx
@@ -1,4 +1,3 @@
-import { A } from "@solidjs/router";
 import { For } from "solid-js";
 import { Joke } from "../types";
 
@@ -8,14 +7,23 @@ export type JokeListProps = {
 
 export function JokeList(props: JokeListProps) {
 	return (
-		<ul>
+		<div class="accordion" id="jokesAccordion">
 			<For each={props.jokes}>
 				{(joke: Joke) =>
-					<li>
-						<A href={`/jokes/${joke.id}`}>{joke.id}: {joke.setup}</A>
-					</li>
+					<div class="accordion-item">
+						<h2 class="accordion-header" id={`heading_${joke.id}`}>
+							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse_${joke.id}`} aria-expanded="false" aria-controls={`collapse_${joke.id}`}>
+								{joke.setup}
+							</button>
+						</h2>
+						<div id={`collapse_${joke.id}`} class="accordion-collapse collapse" aria-labelledby={`heading_${joke.id}`} data-bs-parent="#jokesAccordion">
+							<div class="accordion-body">
+								<strong>{joke.punchline}</strong>
+							</div>
+						</div>
+					</div>
 				}
 			</For>
-		</ul>
+		</div>
 	);
 }
